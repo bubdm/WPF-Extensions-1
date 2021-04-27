967b9e5b7308aff570da7cde60395275c597a57c
Yannick Schrade <yannick1218@outlook.com>
2021-04-27 21:08:25 +0200

    Added CommandButtons & TitleBar to AdvancedWindow
    changes:
    - Added DependencyProperty for TitleBar to Advanced Window
    - Added DependencyProperty for CommandButtons to Advanced Window
    - Added DefaultTitleBar
    - Added DefaultCommandButtons
    
    issue:

--- src/WPF-Extension.Controls/AdvancedWindow.cs
+++ src/WPF-Extension.Controls/AdvancedWindow.cs
@@ -1,29 +1,32 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Drawing;
+using System.Reflection;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using ControlzEx.Behaviors;
+using ControlzEx.Native;
+using ControlzEx.Standard;
 using Microsoft.Xaml.Behaviors;
+using Brush = System.Windows.Media.Brush;
+using Color = System.Windows.Media.Color;
+using ColorConverter = System.Windows.Media.ColorConverter;
 
 namespace WPF_Extensions.Controls
 {
-    
+    [TemplatePart(Name = "PART_CommandButtons", Type = typeof(UIElement))]
+    [TemplatePart(Name = "PART_TitleBar", Type = typeof(UIElement))]
     public class AdvancedWindow : Window
     {
 
+        private static readonly PropertyInfo _criticalHandlePropertyInfo = typeof(Window).GetProperty("CriticalHandle", BindingFlags.NonPublic | BindingFlags.Instance);
+        private static readonly object[] _emptyObjectArray = Array.Empty<object>();
+
         #region DependencyProperties
-
-
 
         public Thickness ResizeBoarderThickness
         {
@@ -57,7 +60,7 @@ namespace WPF_Extensions.Controls
         public static readonly DependencyProperty KeepBorderOnMaximizeProperty =
             DependencyProperty.Register(nameof(KeepBorderOnMaximize), typeof(bool), typeof(AdvancedWindow), new PropertyMetadata(false));
 
-        
+
 
         public bool TryToBeFlickerFree
         {
@@ -94,7 +97,7 @@ namespace WPF_Extensions.Controls
 
         // Using a DependencyProperty as the backing store for NonActiveWindowGlow.  This enables animation, styling, binding, etc...
         public static readonly DependencyProperty NonActiveWindowGlowProperty =
-            DependencyProperty.Register("NonActiveWindowGlow", typeof(Brush), typeof(AdvancedWindow), new PropertyMetadata(new SolidColorBrush((Color)ColorConverter.ConvertFromString("#3E3E42"))));   
+            DependencyProperty.Register("NonActiveWindowGlow", typeof(Brush), typeof(AdvancedWindow), new PropertyMetadata(new SolidColorBrush((Color)ColorConverter.ConvertFromString("#3E3E42"))));
 
 
 
@@ -121,7 +124,44 @@ namespace WPF_Extensions.Controls
             DependencyProperty.Register("ShowMaxRestoreButton", typeof(bool), typeof(AdvancedWindow), new PropertyMetadata(true));
 
 
+        public UIElement TitleBar
+        {
+            get => (UIElement)GetValue(TitleBarProperty);
+            set => SetValue(TitleBarProperty, value);
+        }
 
+        // Using a DependencyProperty as the backing store for TitleBar.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty TitleBarProperty =
+            DependencyProperty.Register("TitleBar", typeof(UIElement), typeof(AdvancedWindow), new PropertyMetadata());
+
+
+        public UIElement CommandButtons
+        {
+            get => (UIElement)GetValue(CommandButtonsProperty);
+            set => SetValue(CommandButtonsProperty, value);
+        }
+
+        // Using a DependencyProperty as the backing store for CommandButtons.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty CommandButtonsProperty =
+            DependencyProperty.Register("CommandButtons", typeof(UIElement), typeof(AdvancedWindow), new PropertyMetadata());
+
+        #region TitleBar
+
+
+
+        public Brush TitleBarBackground
+        {
+            get => (Brush)GetValue(TitleBarBackgroundProperty);
+            set => SetValue(TitleBarBackgroundProperty, value);
+        }
+
+        // Using a DependencyProperty as the backing store for TitleBarBackground.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty TitleBarBackgroundProperty =
+            DependencyProperty.Register("TitleBarBackground", typeof(Brush), typeof(AdvancedWindow), new PropertyMetadata(null));
+
+
+
+        #endregion
 
         #endregion
 
@@ -132,11 +172,96 @@ namespace WPF_Extensions.Controls
             BorderThicknessProperty.OverrideMetadata(typeof(AdvancedWindow), new FrameworkPropertyMetadata(new Thickness(1)));
             WindowStyleProperty.OverrideMetadata(typeof(AdvancedWindow), new FrameworkPropertyMetadata(WindowStyle.None));
             AllowsTransparencyProperty.OverrideMetadata(typeof(AdvancedWindow), new FrameworkPropertyMetadata(false));
+            BackgroundProperty.OverrideMetadata(typeof(AdvancedWindow), new FrameworkPropertyMetadata(new SolidColorBrush(Colors.White)));
         }
 
         public AdvancedWindow()
         {
+            ContentRendered += OnContentRendered;
+
             InitializeWindowChrome();
+        }
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+
+            
+            if(TitleBarBackground == null)
+            {
+                BindingOperations.SetBinding(this, TitleBarBackgroundProperty, new Binding { Path = new PropertyPath(TitleBarBackgroundProperty), Source = this });
+            }
+
+            if(TitleBar is null)
+            {
+                var titlebar = new DefaultTitleBar();
+                TitleBar = titlebar;
+                BindingOperations.SetBinding(TitleBar, DefaultTitleBar.TitleProperty, new Binding { Path = new PropertyPath(TitleProperty), Source = this });
+                BindingOperations.SetBinding(TitleBar, DefaultTitleBar.IconProperty, new Binding { Path = new PropertyPath(IconProperty), Source = this });
+                
+                Icon defaulticon = SystemIcons.Application;
+                BitmapSource imgsource = Imaging.CreateBitmapSourceFromHIcon(defaulticon.Handle, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions());
+                
+                titlebar.Icon = Icon is null ? imgsource : Icon;
+            }
+
+            if(CommandButtons is null)
+            {
+                CommandButtons = new DefaultCommandButtons();
+                
+            }
+
+
+            var titleBar = GetTemplateChild("Titlebar") as FrameworkElement;
+            titleBar.MouseLeftButtonDown += TitleBar_MouseLeftButtonDown;
+
+        }
+
+#pragma warning disable 618
+        private void TitleBar_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ClickCount == 1)
+            {
+                e.Handled = true;
+
+                // taken from DragMove internal code
+                VerifyAccess();
+
+                // for the touch usage
+                UnsafeNativeMethods.ReleaseCapture();
+
+                var criticalHandle = (IntPtr)_criticalHandlePropertyInfo.GetValue(this, _emptyObjectArray);
+
+                var wpfPoint = PointToScreen(Mouse.GetPosition(this));
+                var x = (int)wpfPoint.X;
+                var y = (int)wpfPoint.Y;
+                NativeMethods.SendMessage(criticalHandle, WM.NCLBUTTONDOWN, (IntPtr)HT.CAPTION, new IntPtr(x | (y << 16)));
+            }
+            else if (e.ClickCount == 2
+                     && ResizeMode != ResizeMode.NoResize)
+            {
+                e.Handled = true;
+
+                if (WindowState == WindowState.Normal
+                    && ResizeMode != ResizeMode.NoResize
+                    && ResizeMode != ResizeMode.CanMinimize)
+                {
+                    SystemCommands.MaximizeWindow(this);
+                }
+                else
+                {
+                    SystemCommands.RestoreWindow(this);
+                }
+            }
+        }
+#pragma warning restore 618
+
+
+        private void OnContentRendered(object sender, EventArgs e)
+        {
+            ContentRendered -= OnContentRendered;
+
+            InitializeGlowWindowBehavior();
         }
 
         private void InitializeWindowChrome()
@@ -148,6 +273,16 @@ namespace WPF_Extensions.Controls
             BindingOperations.SetBinding(behavior, WindowChromeBehavior.TryToBeFlickerFreeProperty, new Binding { Path = new PropertyPath(TryToBeFlickerFreeProperty), Source = this });
             BindingOperations.SetBinding(behavior, WindowChromeBehavior.EnableMinimizeProperty, new Binding { Path = new PropertyPath(ShowMinButtonProperty), Source = this });
             BindingOperations.SetBinding(behavior, WindowChromeBehavior.EnableMaxRestoreProperty, new Binding { Path = new PropertyPath(ShowMaxRestoreButtonProperty), Source = this });
+
+            Interaction.GetBehaviors(this).Add(behavior);
+        }
+
+        private void InitializeGlowWindowBehavior()
+        {
+            var behavior = new GlowWindowBehavior();
+            BindingOperations.SetBinding(behavior, GlowWindowBehavior.ResizeBorderThicknessProperty, new Binding { Path = new PropertyPath(ResizeBoarderThicknessProperty), Source = this });
+            BindingOperations.SetBinding(behavior, GlowWindowBehavior.GlowBrushProperty, new Binding { Path = new PropertyPath(WindowGlowProperty), Source = this });
+            BindingOperations.SetBinding(behavior, GlowWindowBehavior.NonActiveGlowBrushProperty, new Binding { Path = new PropertyPath(NonActiveWindowGlowProperty), Source = this });
 
             Interaction.GetBehaviors(this).Add(behavior);
         }
---
+++ src/WPF-Extension.Controls/DefaultCommandButtons.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace WPF_Extensions.Controls
+{
+    public class DefaultCommandButtons : UserControl
+    {
+        private Button _maxRestoreButton;
+        private Window _parentWindow;
+
+        static DefaultCommandButtons()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(DefaultCommandButtons), new FrameworkPropertyMetadata(typeof(DefaultCommandButtons)));
+        }
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+
+            _parentWindow = GetParentWindow();
+
+            var closeButton = GetTemplateChild("CloseButton") as Button;
+            var minimizeButton = GetTemplateChild("MinimizeButton") as Button;
+            _maxRestoreButton = GetTemplateChild("MaxRestoreButton") as Button;
+
+            closeButton.Click += (s, e) => _parentWindow?.Close();
+            minimizeButton.Click += (s, e) => _parentWindow.WindowState = WindowState.Minimized;
+            _maxRestoreButton.Click += (s, e) => _parentWindow.WindowState = _parentWindow.WindowState == WindowState.Maximized ? WindowState.Normal : WindowState.Maximized;
+
+
+            _parentWindow.StateChanged += (s, e) =>
+            {
+                switch (_parentWindow.WindowState)
+                {
+                    case WindowState.Normal:
+                        _maxRestoreButton.Content = "\uE739";
+                        break;
+                    case WindowState.Maximized:
+                        _maxRestoreButton.Content = "\uE923";
+                        break;
+                    default:
+                        break;
+                }
+            };
+        }
+
+
+        private Window GetParentWindow()
+        {
+            var window = Window.GetWindow(this);
+
+            if (window is not null)
+            {
+                return window;
+            }
+
+            var parent = VisualTreeHelper.GetParent(this);
+            Window parentWindow = null;
+
+            while (parent is not null
+                && (parentWindow = parent as Window) is null)
+            {
+                parent = VisualTreeHelper.GetParent(parent);
+            }
+
+            return parentWindow;
+        }
+    }
+}
